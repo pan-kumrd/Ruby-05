586a665ec7ce3ee08450b5d131e5f1cf91459753
Michael Ruml <ruml@mail.muni.cz>
2015-12-12 17:41:15 +0100

    Add default admin@michaelruml.cz user with password 12345678

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+role = Role.find_or_create_by({ name: "admin" })
+
+admin = User.new email: "admin@michaelruml.cz",
+                 password: "12345678",
+                 password_confirmation: "12345678",
+                 admin: true
+admin.skip_confirmation!
+admin.roles << role
+admin.save
